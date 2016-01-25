858f46c8eff7f993cda4a3a689567dd635ae6838
rzelayafavila <Rene.Zelaya.F@gmail.com>
2016-01-25 17:15:06 -0500

    Registering CrossRefDBResource so it gets added to the API urls/endpoints.

--- tribe/urls.py
+++ tribe/urls.py
@@ -12,6 +12,7 @@ from genesets.api.resources import GenesetResource
 from genesets.api.resources import VersionResource
 from genesets.api.resources import PublicationResource
 from genesets.api.resources import CrossrefResource
+from genesets.api.resources import CrossrefDBResource
 
 v1_api = Api()
 v1_api.register(UserResource())
@@ -21,6 +22,7 @@ v1_api.register(GenesetResource())
 v1_api.register(VersionResource())
 v1_api.register(PublicationResource())
 v1_api.register(CrossrefResource())
+v1_api.register(CrossrefDBResource())
 
 urlpatterns = patterns('',
     url(r'^$', ensure_csrf_cookie(TemplateView.as_view(template_name="index.html")), name='home'),
@@ -49,5 +51,3 @@ def handler500(request):
    return HttpResponseServerError(t.render(Context({
        'request': request,
    })))
-
-
