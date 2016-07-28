4bda0ba7ecc63c9dd0ce1655398ccd3202c54c65
Rene A. Zelaya <4641983+rzelayafavila@users.noreply.github.com>
2016-07-28 14:18:26 -0400

    Modifying Genes and Genesets search_indexes.py (the only 2 apps for which we have made search indexes) so that they extend celery_haystack's CelerySearchIndex instead of the default ModelSearchIndex. This is so that these search indexes can be updated via the celery_haystack queue system. Also removing unnecessary imports, improving documentation, and making these files PEP8 compliant.

--- genesets/search_indexes.py
+++ genesets/search_indexes.py
@@ -1,12 +1,13 @@
-
 import re
 
+from celery_haystack.indexes import CelerySearchIndex
 from haystack import indexes
 from genesets.models import Geneset
 
 NONWORD = re.compile('\W+')
 
-class GenesetIndex(indexes.ModelSearchIndex, indexes.Indexable):
+
+class GenesetIndex(CelerySearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
 
     class Meta:
@@ -15,13 +16,18 @@ class GenesetIndex(indexes.ModelSearchIndex, indexes.Indexable):
     def prepare(self, object):
         self.prepared_data = super(GenesetIndex, self).prepare(object)
 
-        # Make a txt field that contains the title and abstract split by non-words
-        # This makes indexing things like GO-BP:... easier.
+        # Make a txt field that contains the title and abstract split
+        # by non-words.
+        # This makes indexing things like "GO-BP-<ID#>:<GO title>"" easier.
         txts = NONWORD.split(object.title)
 
         # Add some logic in case the geneset has no abstract
         # otherwise haystack breaks when trying to index it:
         if object.abstract:
             txts.extend(NONWORD.split(object.abstract))
-        self.prepared_data['text'] = self.prepared_data['text'] + ' ' + ' '.join(txts)
+        self.prepared_data['text'] = self.prepared_data['text'] + ' ' + \
+            ' '.join(txts)
         return self.prepared_data
+
+    def get_model(self):
+        return Geneset
