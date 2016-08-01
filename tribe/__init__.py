17435bf9ec7161ffcf4a66bc19d0a6f271dbb7d7
Rene A. Zelaya <4641983+rzelayafavila@users.noreply.github.com>
2016-08-01 12:08:00 -0400

    Adding try...except statements to tribe/__init__.py so that celery app is only set up if celery_haystack is installed.

--- tribe/__init__.py
+++ tribe/__init__.py
@@ -1,10 +1,20 @@
 """
-The next few lines of code are copied from Celery
-documentation for working with Django:
+Check to see if celery_haystack is installed. If it is, set up
+celery app. If not, just pass.
+
+The lines of code that import the celery app are copied
+from the Celery documentation for working with Django:
 http://docs.celeryproject.org/en/latest/django/first-steps-with-django.html
+
 """
 from __future__ import absolute_import
 
-# This will make sure the app is always imported when
-# Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+try:
+    import celery_haystack
+
+    # This will make sure the app is always imported when
+    # Django starts so that shared_task will use this app.
+    from .celery import app as celery_app
+
+except ImportError:
+    pass
