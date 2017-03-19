664974d8d23a6a68d45f61e63c43d6c29431645c
rzelayafavila <Rene.Zelaya.F@gmail.com>
2017-03-19 16:50:41 -0400

    Changing the Genes resoure method (in genes/resource.js) from GET to POST, so that it supports searching around 500 genes at a time or more.

--- interface/src/app/genes/resource.js
+++ interface/src/app/genes/resource.js
@@ -3,13 +3,16 @@
  * The ngResource capabilities encapsulate a lot of the nitty gritty of $http
  * http://docs.angularjs.org/api/ngResource.$resource
  */
-angular.module("tribe.gene.resource", ['ngResource', 'ngRoute'])
-    .factory('Genes', ['$resource', '$http', function($resource, $http) {
-        return $resource('/api/v1/gene/:id', {id: '@id'}, {
-            search: {
-                url: '/api/v1/gene/search',
-                method: 'GET',
-                isArray: true
-            }
-        });
-    }]);
+angular.module('tribe.gene.resource', ['ngResource'])
+
+.factory('Genes', ['$resource', function($resource) {
+  return $resource('/api/v1/gene/:id', {id: '@id'}, {
+
+    search: {
+      url: '/api/v1/gene/search',
+      method: 'POST',
+      isArray: true
+    }
+
+  });
+}]);
