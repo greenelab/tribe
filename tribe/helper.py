e2c9d1cebdd96c0ff45976ff98abf62d5cf0f450
Dongbo <dongbo.hu@gmail.com>
2018-10-25 17:05:55 -0400

    Tweak comments

--- tribe/helper.py
+++ tribe/helper.py
@@ -1,22 +1,23 @@
-"""This file includes some helper function."""
+"""This file includes some helper functions."""
 
 from ConfigParser import RawConfigParser
 from os.path import join, normpath
 
 
 def get_config(config_dir, filename):
-    """Parses config_dir/filename and returns the configuration.
+    """Parses <filename> from <config_dir> and returns configuration.
 
-    The ini files are parsed by RawConfigParser (instead of ConfigParser
-    or SafeConfigParser) to avoid "%" interpolation when an option's
-    value includes "%" character (such as password strings).  See more
-    details at: https://docs.python.org/2/library/configparser.html
+    <filename> is assumed to be in `ini` format, and is parsed by
+    RawConfigParser (instead of ConfigParser or SafeConfigParser) to
+    avoid "%" interpolation when an option's value includes "%"
+    character (such as password strings).  Please see more details at:
+    https://docs.python.org/2/library/configparser.html
 
-    If the input file has "[include]" section, also reads that included
+    If the input file has "[include]" section, also read that included
     file from config_dir directory.  The final configuration will be a
-    combination of these two files.  If an option is specified in both
-    the input file and the config file, the former will always take
-    precedence over the latter.
+    combination of these two files.  For options that are specified in
+    both <filename> and the included file, values in <filename> always
+    take precedence.
     """
     secrets = RawConfigParser()
     secrets.read(normpath(join(config_dir, filename)))
