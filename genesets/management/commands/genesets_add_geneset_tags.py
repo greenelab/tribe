5d04617078f0cf43bdc5d9042ef73b00fc664eaf
Dongbo <dongbo.hu@gmail.com>
2019-02-19 16:45:42 -0500

    Update django-genes version requirement, replace make_option with parser.add_argument

--- genesets/management/commands/genesets_add_geneset_tags.py
+++ genesets/management/commands/genesets_add_geneset_tags.py
@@ -11,27 +11,74 @@ from django.contrib.auth.models import User
 from organisms.models import Organism
 from genesets.models import Geneset
 
-from optparse import make_option
-
 # This dictionary serves two purposes: To abbreviate the tag name that has to be
 # entered in the command line, and to make sure users enter an accurate tag name.
 TAG_NAMES = {
     't-s': 'tissue-specific'
 }
 
-class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option('--tax_id', dest='tax_id', help='Taxonomy ID assigned by NCBI to this organism. This field is optional. If not provided, tags will be applied to genesets of all organisms.'),
-        make_option('--tag_file', dest='tag_file', help='File mapping tags to genesets'),
-        make_option('--geneset_id_column', dest='geneset_column', help="Geneset Identification column (Can be GO, KEGG, or DO id #s)."),
-        make_option('--primary_tag_column', dest='tag_column', help='Primary tag column'),
-        make_option('--secondary_tag_column', dest='second_tag_column', help='Secondary tag column. This field is optional.'),
-        make_option('--gs_name_column', dest='gs_name_column', help='Geneset name column'),
-        make_option('--additional_tag', dest='additional_tag', help='Code for additional tag name (such as "t-s" for "tissue-specific"). This field is optional.'),
-        make_option('--header_present', dest='header', action='store_true', help='Optional flag, stating a header is present in the tag mapping file'),
-    )
 
+class Command(BaseCommand):
     help = 'Management command to add geneset tags.'
+
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--tax_id',
+            dest='tax_id',
+            required=True,
+            help=('Taxonomy ID assigned by NCBI to this organism. ' +
+                  'This field is optional. If not provided, tags will be ' +
+                  'applied to genesets of all organisms.')
+        )
+
+        parser.add_argument(
+            '--tag_file',
+            dest='tag_file',
+            required=True,
+            help='File mapping tags to genesets'
+        )
+
+        parser.add_argument(
+            '--geneset_id_column',
+            dest='geneset_column',
+            required=True,
+            help="Geneset Identification column (Can be GO, KEGG, or DO id #s)."
+        )
+
+        parser.add_argument(
+            '--primary_tag_column',
+            dest='tag_column',
+            required=True,
+            help='Primary tag column'
+        )
+
+        parser.add_argument(
+            '--secondary_tag_column',
+            dest='second_tag_column',
+            required=True,
+            help='Secondary tag column. This field is optional.'
+        )
+
+        parser.add_argument(
+            '--gs_name_column',
+            dest='gs_name_column',
+            required=True,
+            help='Geneset name column'
+        )
+
+        parser.add_argument(
+            '--additional_tag',
+            dest='additional_tag',
+            help=('Code for additional tag name (such as "t-s" for ' +
+                  '"tissue-specific"). This field is optional.')
+        )
+
+        parser.add_argument(
+            '--header_present',
+            dest='header',
+            action='store_true',
+            help='Optional flag, stating a header is present in the tag mapping file'
+        )
 
     def handle(self, *args, **options):
 
@@ -63,22 +110,33 @@ class Command(BaseCommand):
             gs_id = toks[int(geneset_column)].split(':')
             gs_tag = toks[int(tag_column)]
             gs_name = toks[int(gs_name_column)]
-            gs_name = gs_name.replace('_', ' ') # Underscores may be used in files in place of spaces
+            # Underscores may be used in files in place of spaces
+            gs_name = gs_name.replace('_', ' ')
 
-            if (len(gs_id) == 1): # If Id column was not able to be split in two by ':', state this term was not found
-                print('Geneset ' + str(toks[int(geneset_column)]) + '-' + str(gs_name) + ' and organism ' + str(org) + ' does not exist in our database')
+            # If Id column was not able to be split in two by ':', state this
+            # term was not found
+            if (len(gs_id) == 1):
+                print('Geneset ' + str(toks[int(geneset_column)]) + '-' +
+                      str(gs_name) + ' and organism ' + str(org) +
+                      ' does not exist in our database'
+                )
                 continue
 
-
-            # Search for genesets with specific ID and geneset name ('icontains' makes it case-insensitive)
-            gs_qset = Geneset.objects.filter(title__startswith=gs_id[0][:2]).filter(title__contains=gs_id[1]).filter(title__icontains=gs_name)
+            # Search for genesets with specific ID and geneset name
+            # ('icontains' makes it case-insensitive)
+            gs_qset = Geneset.objects.filter(
+                title__startswith=gs_id[0][:2]
+            ).filter(title__contains=gs_id[1]).filter(title__icontains=gs_name)
 
             if org:
                 gs_qset = gs_qset.filter(organism=org)
 
-            if not gs_qset.exists(): # If the queryset is empty, mark geneset(s) as not found.
-                print('Geneset ' + str(toks[int(geneset_column)]) + '-' + str(gs_name) + ' and organism ' + str(org) + ' does not exist in our database')
-
+            # If the queryset is empty, mark geneset(s) as not found.
+            if not gs_qset.exists():
+                print('Geneset ' + str(toks[int(geneset_column)]) + '-' +
+                      str(gs_name) + ' and organism ' + str(org) +
+                      ' does not exist in our database'
+                )
             else:
                 for gs in gs_qset:
                     gs.tags.add(str(gs_tag))
@@ -88,4 +146,3 @@ class Command(BaseCommand):
                         gs.tags.add(str(toks[int(sec_gs_tag)]))
 
         tag_file_fh.close()
-
--- genesets/management/commands/genesets_load_kegg.py
+++ genesets/management/commands/genesets_load_kegg.py
@@ -5,8 +5,6 @@ import sys
 import requests
 
 from collections import defaultdict
-from optparse import make_option
-
 from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
@@ -50,7 +48,7 @@ def get_kegg_members(kegg_base, organism, record_type):
     for line in kegg_iter:
         toks = line.split()
         group = toks[0].split(':')[1]    # group listed first, has prefix
-        geneid = toks[1].split(':')[1]  # gene listed second, has prefix
+        geneid = toks[1].split(':')[1]   # gene listed second, has prefix
         results[group].add(geneid)
     return results
 
@@ -69,14 +67,41 @@ def get_kegg_info(kegg_base, identifier):
     return result
 
 class Command(BaseCommand):
-    option_list = BaseCommand.option_list + (
-        make_option('--organism', dest='organism', help="Organism's full name."),
-        make_option('--user', dest='user', default="tribeupdater", help="Username for the user to be assigned the created geneset/versions."),
-        make_option('--gene_id', dest='gene_id', help="Enter a gene identifier to use (other than Entrez, which is the default) to map genes."),
-        make_option('--kegg_record_types', dest='kegg_record_types', help="Desired KEGG Record Types, if something other than (Pathway, Module, Disease). Each record type should be separated by a comma from the rest."),
+    help = ('Load KEGG annotations for the listed organism and assign them ' +
+            'to the passed user.'
     )
 
-    help = 'Load KEGG annotations for the listed organism and assign them to the passed user.'
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--organism',
+            dest='organism',
+            help="Organism's full name."
+        )
+
+        parser.add_argument(
+            '--user',
+            dest='user',
+            default="tribeupdater",
+            help="Username assigned to the created geneset/versions."
+        )
+
+        parser.add_argument(
+            '--gene_id',
+            dest='gene_id',
+            help=("Enter a gene identifier to use (other than Entrez, which " +
+                  "is the default) to map genes."
+            )
+        )
+
+        parser.add_argument(
+            '--kegg_record_types',
+            dest='kegg_record_types',
+            help=("Desired KEGG Record Types, if something other than " +
+                  "(Pathway, Module, Disease). Each record type should be " +
+                  "separated by a comma from the rest."
+            )
+        )
+
     def handle(self, *args, **options):
 
         user_name = options.get('user')
@@ -84,26 +109,44 @@ class Command(BaseCommand):
         try:
             user = User.objects.get(username = user_name)
         except User.DoesNotExist:
-            logger.error('The user %s did not exist.', user_name, extra={'options': options})
+            logger.error(
+                'The user %s did not exist.',
+                user_name,
+                extra={'options': options}
+            )
             sys.exit()
 
         org = None
         try:
-            org = Organism.objects.get(scientific_name = options.get('organism'))
+            org = Organism.objects.get(
+                scientific_name = options.get('organism')
+            )
         except Organism.DoesNotExist:
-            logger.error('The organism %s did not exist.', options.get('organism'), extra={'options': options})
+            logger.error(
+                'The organism %s did not exist.',
+                options.get('organism'),
+                extra={'options': options}
+            )
             sys.exit()
 
         version = get_kegg_version(KEGG_URL_BASE)
         if version is None:
-            logger.error('The KEGG api may have changed. Release no longer starts with "Release".')
+            logger.error(
+                'The KEGG api may have changed. '
+                'Release no longer starts with "Release".'
+            )
         else:
             logger.info('Working with KEGG version %s.', version)
 
         if (options.get('kegg_record_types')):
-            kegg_record_types = (options.get('kegg_record_types')).replace(" ", "").split(",")
+            kegg_record_types = (
+                options.get('kegg_record_types')
+            ).replace(" ", "").split(",")
             kegg_record_types = tuple(kegg_record_types)
-            logger.info('Requested KEGG Record Types are: %s', str(kegg_record_types))
+            logger.info(
+                'Requested KEGG Record Types are: %s',
+                str(kegg_record_types)
+            )
 
         else:
             kegg_record_types = KEGG_RECORD_TYPES
@@ -111,13 +154,18 @@ class Command(BaseCommand):
 
         for record_type in kegg_record_types:
             logger.info('Starting record type %s.', record_type)
-            record_members = get_kegg_members(KEGG_URL_BASE, KEGG_NAMES[org.scientific_name], record_type)
+            record_members = get_kegg_members(
+                KEGG_URL_BASE, KEGG_NAMES[org.scientific_name], record_type
+            )
             for (record, members) in record_members.iteritems():
                 if record_type == 'Module':
-                    record = record.split('_').pop() #for modules, they are prefixed with species_
+                    # for modules, they are prefixed with species_
+                    record = record.split('_').pop()
                 slug = slugify(org.scientific_name + ' ' + record)
                 gs_info = get_kegg_info(KEGG_URL_BASE, record)
-                gs_info['title'] = 'KEGG-' + record_type + '-' + record + ': ' + gs_info['title'] #make title more search friendly
+                gs_info['title'] = (
+                    'KEGG-' + record_type + '-' + record + ': ' + gs_info['title']
+                )  # make title more search friendly
                 try:
                     geneset = Geneset.objects.get(slug=slug)
                     changed = False
@@ -131,22 +179,36 @@ class Command(BaseCommand):
                         geneset.save()
 
                 except Geneset.DoesNotExist:
-                    geneset = Geneset(creator=user, title=gs_info['title'], organism=org, abstract=gs_info['abstract'], slug=slug, public=True)
+                    geneset = Geneset(
+                        creator=user,
+                        title=gs_info['title'],
+                        organism=org,
+                        abstract=gs_info['abstract'],
+                        slug=slug,
+                        public=True
+                    )
                     geneset.save()
 
                 if (options.get('gene_id')):
                     gene_id = options.get('gene_id')
                     if (gene_id == 'systematic_name'):
-                        annots = frozenset([(gene.pk, None) for gene in Gene.objects.filter(systematic_name__in=members)])
+                        annots = frozenset(
+                            [(gene.pk, None) for gene in Gene.objects.filter(systematic_name__in=members)]
+                        )
                     elif (gene_id == 'standard_name'):
-                        annots = frozenset([(gene.pk, None) for gene in Gene.objects.filter(standard_name__in=members)])
+                        annots = frozenset(
+                            [(gene.pk, None) for gene in Gene.objects.filter(standard_name__in=members)]
+                        )
                     else:
                         logger.error('gene_id entered is not supported (yet)')
                         return False
                 else:
-                    annots = frozenset([(gene.pk, None) for gene in Gene.objects.filter(entrezid__in=members)])
+                    annots = frozenset(
+                        [(gene.pk, None) for gene in Gene.objects.filter(entrezid__in=members)]
+                    )
 
-                most_recent_versions = Version.objects.filter(geneset=geneset).order_by('-commit_date')[:1]
+                most_recent_versions = Version.objects.filter(
+                    geneset=geneset).order_by('-commit_date')[:1]
                 description = ''
                 most_recent_version = None
                 if most_recent_versions:
@@ -156,13 +218,23 @@ class Command(BaseCommand):
                     new = annots - most_recent_version.annotations
                     removed = most_recent_version.annotations - annots
                     if (new or removed):
-                        description = description + 'Added ' + str(len(new)) + ' and removed ' + str(len(removed)) + ' annotations from KEGG version ' + version + '.'
+                        description = (
+                            description + 'Added ' + str(len(new)) +
+                            ' and removed ' + str(len(removed)) +
+                            ' annotations from KEGG version ' + version + '.'
+                        )
                 else:
-                    description = 'Created with ' + str(len(annots)) + ' annotations from KEGG version ' + version + '.'
+                    description = (
+                        'Created with ' + str(len(annots)) +
+                        ' annotations from KEGG version ' + version + '.'
+                    )
                 if description:
-                    v_obj = Version(geneset=geneset, creator=user, parent=most_recent_version, commit_date=timezone.now())
+                    v_obj = Version(
+                        geneset=geneset,
+                        creator=user,
+                        parent=most_recent_version,
+                        commit_date=timezone.now()
+                    )
                     v_obj.description = description
                     v_obj.annotations = annots
                     v_obj.save()
-
-
